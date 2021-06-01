f90df1e1cc441a611b314b32297792d89df63d06
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2021-06-01 18:43:04 +0900

    fix: refactor onResolve func

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -10,19 +10,18 @@ export const unpkgPathPlugin = (inputCode: string) => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
-      build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResole', args);
-        if (args.path === 'index.js') {
-          return { path: args.path, namespace: 'a'};
-        }
+      build.onResolve({ filter: /(^index\.js$})/ }, () => {
+        return { path: 'index.js', namespace: 'a'}
+      });
 
-        if (args.path.includes('./') || args.path.includes('../')) {
-          return {
+      build.onResolve({ filter: /^\.+\// }, (args:any) => {
+        return {
           namespace: 'a',
           path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/').href
-          };
-        }
+        };
+      });
 
+      build.onResolve({ filter: /.*/ }, async (args: any) => {
         return {
           namespace: 'a',
           path: `https://unpkg.com/${args.path}`
@@ -30,8 +29,6 @@ export const unpkgPathPlugin = (inputCode: string) => {
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log('onLoad', args);
-
         if (args.path === 'index.js') {
           return {
             loader: 'jsx',
