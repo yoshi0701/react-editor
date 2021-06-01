7f0abb4b115c45c212364f7974acbd5098efdf00
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2021-06-02 01:16:34 +0900

    feat: error handling added

--- src/index.tsx
+++ src/index.tsx
@@ -51,7 +51,13 @@ const App = () => {
       <div id="root"></div>
       <script>
         window.addEventListener('message', (event) => {
-          eval(event.data);
+          try {
+            eval(event.data);  
+          } catch(e) {
+            const root = document.querySelector('#root');
+            root.innerHTML = '<div style="color: red"><h4>Runtime Error</h4>' + e + '</div>'
+            console.error(e);
+          }
         }, false);
       </script>
     </body>
