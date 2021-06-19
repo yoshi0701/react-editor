93c9050e5330e8550b613ef0a0d1d4de8d67a1a5
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2021-06-20 03:57:58 +0900

    fix: check whether preview mode or not

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,8 +1,38 @@
+import {useState, useEffect, useRef} from 'react';
 import MDEditor from '@uiw/react-md-editor'
 
 const TextEditor: React.FC = () => {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const [editing, setEditing] = useState(false)
+
+  // check whether click inside or outside of editor area
+  // via listening event switching preview mode or editing mode
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      if(ref.current && event.target && ref.current.contains(event.target as Node)) {
+        // if event is inside of editor just return
+        return;
+      }
+      // outside of editor setEditing is false
+      setEditing(false);
+    };
+    document.addEventListener('click', listener, { capture: true})
+
+    return () => {
+      document.removeEventListener('click', listener, { capture: true})
+    };
+  }, [])
+
+  if (editing) {
+    return (
+      <div ref={ref}>
+        <MDEditor />
+      </div>
+    )
+  }
+
   return (
-    <div>
+    <div onClick={() => setEditing(true)}>
       <MDEditor.Markdown  source={'# Header'}/>
     </div>
   )
