b4d45e2d85ff54b0f890fef1013bca1d8f434c45
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2021-06-23 03:44:56 +0900

    wip: implementing redux for state management

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,19 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { store } from './state';
+
 // import CodeCell from './components/code-cell';
 import TextEditor from './components/text-editor';
 
 const App = () => {
-  return <div>
-    <TextEditor />
-  </div>
+  return (
+    <Provider store={store}>
+      <div>
+        <TextEditor />
+      </div>
+    </Provider>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
